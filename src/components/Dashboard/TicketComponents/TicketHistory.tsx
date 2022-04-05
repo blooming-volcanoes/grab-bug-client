ab547d0c9165b65235449c60e9c407fff61c0714
Raiyan <raiyan5764@gmail.com>
2022-04-05 20:31:19 +0600

    [EDITED]- ticketDetails edited

---
+++ src/components/Dashboard/TicketComponents/TicketHistory.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TicketHistory() {
+    return <div>TicketHistory</div>;
+}
+
+export default TicketHistory;
--- src/pages/dashboard/ticketDetails.tsx
+++ src/pages/dashboard/ticketDetails.tsx
@@ -1,11 +1,20 @@
-import TicketComments from "components/Dashboard/TicketComments";
-import TicketSingleDetails from "components/Dashboard/TicketSingleDetails";
+import TicketComments from "components/Dashboard/TicketComponents/TicketComments";
+import TicketSingleDetails from "components/Dashboard/TicketComponents/TicketSingleDetails";
 import React from "react";
 
 function TaskDetails() {
     return (
         <section className="min-h-screen bg-gray-100">
             <div className="gird-cols-1 mx-4 grid gap-y-4 gap-x-4 py-10 md:mx-auto md:max-w-3xl lg:mx-auto lg:max-w-6xl lg:grid-cols-12">
+                {/* details side */}
+                <div className="border-r pr-2 lg:col-span-6">
+                    <TicketSingleDetails />
+                </div>
+
+                {/* comment side */}
+                <div className="lg:col-span-6">
+                    <TicketComments />
+                </div>
                 {/* details side */}
                 <div className="border-r pr-2 lg:col-span-6">
                     <TicketSingleDetails />
