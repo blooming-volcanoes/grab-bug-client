32973265d22cafbaa8f65e2a2144444e6509d166
Raiyan <raiyan5764@gmail.com>
2022-04-06 16:28:50 +0600

    [EDITED]-ticketEdit edited by raiyan

--- src/components/Dashboard/TicketComponents/TicketEdit.tsx
+++ src/components/Dashboard/TicketComponents/TicketEdit.tsx
@@ -1,8 +1,64 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import Input from "components/molecules/Input/Input";
+import SelectInputs from "components/molecules/SelectInputs/SelectInputs";
 import React from "react";
+
+interface ITextnputs {
+    title: string;
+    placeholder?: string;
+    type?: string;
+}
+interface ISelectInputs {
+    title: string;
+    options: string[];
+    values: string[];
+}
+
+const textInputs: ITextnputs[] = [
+    {
+        title: "Title",
+        placeholder: "Your project title",
+        type: "text",
+    },
+    {
+        title: "Description",
+        placeholder: "Your project description",
+        type: "text",
+    },
+];
+
+const selecTypeInputs: ISelectInputs[] = [
+    {
+        title: "Project",
+        options: ["Demo Project 1", "Demo project 2", "Demo project 2"],
+        values: ["Demo Project 1", "Demo project 2", "Demo project 2"],
+    },
+    {
+        title: "Assigned Developer",
+        options: ["Dev Musk", "Elon Musk", "Raiyan"],
+        values: ["Dev Musk", "Elon Musk", "Raiyan"],
+    },
+    {
+        title: "Ticket Priority",
+        options: ["Medium", "High", "Average"],
+        values: ["Medium", "High", "Average"],
+    },
+    {
+        title: "Ticket Status",
+        options: ["In Progress", "Done", "not sure"],
+        values: ["In Progress", "Done", "not sure"],
+    },
+    {
+        title: "Ticket Type",
+        options: ["Bugs/Error", "Image Breaking", "Data flow"],
+        values: ["Bugs/Error", "Image Breaking", "Data flow"],
+    },
+];
 
 function TicketEdit() {
     return (
-        <div className="rounded-lg   bg-white p-4 shadow">
+        <div className="mx-auto rounded-lg bg-white p-4  py-10 shadow lg:max-w-5xl">
             <div className="space-y-2  rounded-lg bg-indigo-500 p-4 capitalize text-white shadow-2xl">
                 {/* ticket Edit header */}
                 <h4 className="text-lg">Edit Ticket</h4>
@@ -10,108 +66,96 @@ function TicketEdit() {
             </div>
 
             {/* commenter details */}
-            <div className="pt-3">
-                <div className="flex flex-col">
-                    <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-                        <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-                            <div className="overflow-hidden">
-                                <table className="table-auto ">
-                                    <thead className="border-b uppercase">
-                                        <tr className="">
-                                            <th scope="col" className="table-th">
-                                                Title
-                                            </th>
-                                            <th scope="col" className="table-th">
-                                                Description
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr className="border-b">
-                                            <input
-                                                className="border-dark-500 mr-5 w-96 border-0 border-b-2"
-                                                type="text"
-                                                placeholder="Enter here"
-                                            />
-                                            <input
-                                                className="border-dark-500 ml-5 w-96 border-0 border-b-2"
-                                                type="text"
-                                                placeholder="Enter here"
-                                            />
-                                        </tr>
-                                        <tr className="border-b text-left">
-                                            <td className="table-td">Project</td>
-                                            <td className="table-td">Assigned</td>
-                                        </tr>
-                                        <tr className="border-b">
-                                            <select className="mr-5 w-96">
-                                                <option value="volvo">Demo Project one</option>
-                                                <option value="saab">Demo Project two</option>
-                                                <option value="fiat">Demo Project three</option>
-                                                <option value="audi">Demo Project Four</option>
-                                            </select>
-
-                                            <select className="ml-5 w-96">
-                                                <option value="volvo">
-                                                    Demo Project Description
-                                                </option>
-                                                <option value="saab">
-                                                    Demo Project Description
-                                                </option>
-                                                <option value="fiat">
-                                                    Demo Project Description
-                                                </option>
-                                                <option value="audi">
-                                                    Demo Project Description
-                                                </option>
-                                            </select>
-                                        </tr>
-                                        <tr className="border-b text-left">
-                                            <td className="table-td">Ticket Priority</td>
-                                            <td className="table-td">Assignment Developer</td>
-                                        </tr>
-                                        <tr className="border-b">
-                                            <select className="mr-5 w-96">
-                                                <option value="volvo">Hight</option>
-                                                <option value="saab">Low</option>
-                                                <option value="fiat">Medium</option>
-                                                <option value="audi">Nothing</option>
-                                            </select>
-
-                                            <select className="ml-5 w-96">
-                                                <option value="volvo">Project In Process</option>
-                                                <option value="saab">Project Out Process</option>
-                                                <option value="fiat">
-                                                    Project running Process
-                                                </option>
-                                                <option value="audi">Project Stop Process</option>
-                                            </select>
-                                        </tr>
-                                        <tr className="border-b text-left">
-                                            <td className="table-td">Ticket Type</td>
-                                        </tr>
-                                        <tr className="border-b">
-                                            <select className="ml-5 w-96 text-left">
-                                                <option value="volvo">Bugs/Errors</option>
-                                                <option value="saab">Wrong</option>
-                                                <option value="fiat">day off</option>
-                                                <option value="audi">Nothing</option>
-                                            </select>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
+            <div className="my-5 pt-3">
+                <ul className="flex flex-col justify-between px-4 lg:flex-row lg:space-x-12">
+                    {/* left side input boxes */}
+                    <div className="space-y-6 lg:w-2/4 ">
+                        {/* plain input text */}
+                        {textInputs
+                            .filter((input) => input.title === "Title")
+                            .map((input, i) => (
+                                <li key={i}>
+                                    <label
+                                        className="flex flex-col space-y-2"
+                                        htmlFor={input.title}
+                                    >
+                                        <span className="text-sm font-semibold">{input.title}</span>
+                                        <Input
+                                            placeholder={input.placeholder}
+                                            type={input.type}
+                                            id={input.title}
+                                            className="w-full  border-t-0  border-b-2 border-l-0 border-r-0 text-sm focus:ring-0"
+                                        />
+                                    </label>
+                                </li>
+                            ))}
+                        {/* input type select */}
+                        {selecTypeInputs
+                            .filter(
+                                (input) =>
+                                    input.title !== "Project" &&
+                                    input.title !== "Ticket Priority" &&
+                                    input.title !== "Ticket Type"
+                            )
+                            .map((input, i) => (
+                                <li key={i}>
+                                    <label
+                                        className="flex flex-col space-y-2"
+                                        htmlFor={input.title}
+                                    >
+                                        <span className="text-sm font-semibold">{input.title}</span>
+                                        <SelectInputs
+                                            values={input.values}
+                                            className="rounded border-2 text-sm focus:ring-0"
+                                        />
+                                    </label>
+                                </li>
+                            ))}
                     </div>
-                    <div className="flex flex-row">
-                        <button className="primary-btn mr-5 w-64 bg-blue-500" type="button">
-                            Back to List
-                        </button>
-                        <button className="primary-btn ml-5 w-64 bg-green-900" type="button">
-                            UPDATE TICKET
-                        </button>
+                    {/* right side input boxes */}
+                    <div className="mt-4 space-y-6 lg:mt-0 lg:w-2/4 ">
+                        {/* plain input text */}
+                        {textInputs
+                            .filter((input) => input.title === "Description")
+                            .map((input, i) => (
+                                <li key={i}>
+                                    <label
+                                        className="flex flex-col space-y-2"
+                                        htmlFor={input.title}
+                                    >
+                                        <span className="text-sm font-semibold">{input.title}</span>
+                                        <Input
+                                            placeholder={input.placeholder}
+                                            type={input.type}
+                                            id={input.title}
+                                            className="w-full border-t-0  border-b-2 border-l-0 border-r-0 text-sm focus:ring-0"
+                                        />
+                                    </label>
+                                </li>
+                            ))}
+                        {/* input type select */}
+                        {selecTypeInputs
+                            .filter(
+                                (input) =>
+                                    input.title !== "Assigned Developer" &&
+                                    input.title !== "Ticket Status"
+                            )
+                            .map((input, i) => (
+                                <li key={i}>
+                                    <label
+                                        className="flex flex-col space-y-2"
+                                        htmlFor={input.title}
+                                    >
+                                        <span className="text-sm font-semibold">{input.title}</span>
+                                        <SelectInputs
+                                            values={input.values}
+                                            className="rounded border-2 text-sm focus:ring-0"
+                                        />
+                                    </label>
+                                </li>
+                            ))}
                     </div>
-                </div>
+                </ul>
             </div>
         </div>
     );
