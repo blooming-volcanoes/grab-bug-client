4db280dd3d30b38d8c4739f89930d55910177f25
Sushanta Gupta <sushanta.gupta007@gmail.com>
2022-04-06 11:11:40 -0700

    work start

---
+++ src/pages/dashboard/projectDescription.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+// eslint-disable-next-line arrow-body-style
+const projectDescription = () => {
+    return <h1> hello </h1>;
+};
+
+export default projectDescription;
