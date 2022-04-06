3aa2ac1302101ba304e0c51db4b8453629068db1
Sadique <mhsadique1307@gmail.com>
2022-04-06 08:25:04 +0600

    half of user role management page done

--- src/mockData/users.js
+++
@@ -1,9 +0,0 @@
-const users = [
-    { id: 1, name: "A.S.M. Habibullah Sadique" },
-    { id: 2, name: "Awal Hossain" },
-    { id: 3, name: "Sushanto Gupta" },
-    { id: 4, name: "Kamrul Haider Chy" },
-    { id: 5, name: "Tajkier Haque Raiyan" },
-    { id: 6, name: "Mostafa Sujon" },
-];
-export default users;
