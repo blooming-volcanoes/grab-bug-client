25640a387c1c25cc68d6928bc8a5e1907e519df7
Sadique <mhsadique1307@gmail.com>
2022-04-20 13:42:10 +0600

    api integrated to 'Tickets' page. working

---
+++ src/DashboardPages/Tickets/TableHead.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function TableHead() {
+    return (
+        <thead className="text-sx border-b uppercase">
+            <tr>
+                <th scope="col" className="table-th">
+                    Title
+                </th>
+                <th scope="col" className="table-th">
+                    Developer Assigned
+                </th>
+                <th scope="col" className="table-th">
+                    Ticket Priority
+                </th>
+                <th scope="col" className="table-th">
+                    Ticket Status
+                </th>
+                <th scope="col" className="table-th">
+                    Ticket Type
+                </th>
+                <th scope="col" className="table-th">
+                    Created by
+                </th>
+                <th scope="col" className="table-th">
+                    Edit
+                </th>
+            </tr>
+        </thead>
+    );
+}
+
+export default TableHead;
--- src/services/Issue.service.ts
+++ src/services/Issue.service.ts
@@ -7,6 +7,11 @@ class IssueReq {
         const data = await httpReq.post("/issue", payload).then((data) => data);
         return data;
     }
+
+    async getAllIssues() {
+        const data = await httpReq.get("/issue/all").then((data) => data);
+        return data;
+    }
 }
 
 const IssueHttpReq = new IssueReq();
