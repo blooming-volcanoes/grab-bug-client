dcc34bd412df4b2ddc8fdd1645afae1ccf287894
kamrulhaider <kamrulhaider.ssb@gmail.com>
2022-04-21 04:23:29 +0600

    [Fix]-homepage

--- src/DashboardPages/IssueCreate/IssueCreate.tsx
+++ src/DashboardPages/IssueCreate/IssueCreate.tsx
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import IssueHttpReq from "../../services/Issue.service";
 import ProjectHttpReq from "../../services/Project.service";
+import Heading from "./Heading";
 
 function IssueCreate() {
     const [success, setSuccess] = useState<any>();
@@ -34,83 +35,87 @@ function IssueCreate() {
     }, []);
 
     return (
-        <div className="m-4">
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="flex">
-                    <input
-                        style={{ outline: "none" }}
-                        onClick={successTextRemover}
-                        className="mb-3 mr-3 w-2/5 flex-auto border-2 border-solid border-indigo-600 py-2 px-3"
-                        placeholder="Title"
-                        {...register("title")}
-                    />
+        <div className="m-0 md:m-3">
+            <div className="container mx-auto h-max rounded-[3px] shadow-[0_0_10px_#5584AC] md:p-3">
+                <Heading />
+                <form className="mx-1 mt-3" onSubmit={handleSubmit(onSubmit)}>
+                    <div className="flex flex-col md:flex-row">
+                        <input
+                            style={{ outline: "none" }}
+                            onClick={successTextRemover}
+                            className="mb-3 mr-3 w-full flex-auto rounded-[3px] border-2 border-solid py-2 px-3 focus:border-[#22577E] md:w-2/5"
+                            placeholder="Title"
+                            {...register("title")}
+                        />
 
-                    <input
-                        style={{ outline: "none" }}
-                        onClick={successTextRemover}
-                        className="mb-3 mr-3 w-2/5 flex-auto border-2 border-solid border-indigo-600 py-2 px-3"
-                        placeholder="Reporter Name"
-                        {...register("reporterName")}
-                    />
+                        <input
+                            style={{ outline: "none" }}
+                            onClick={successTextRemover}
+                            className="mb-3 mr-3 w-full flex-auto rounded-[3px] border-2 border-solid py-2 px-3 focus:border-[#22577E] md:w-2/5"
+                            placeholder="Reporter Name"
+                            {...register("reporterName")}
+                        />
 
-                    <input
-                        style={{ outline: "none" }}
-                        onClick={successTextRemover}
-                        className="mb-3 ml-3 w-2/5 flex-auto border-2 border-solid border-indigo-600 py-2 px-3"
-                        placeholder="Bug Category"
-                        {...register("bugCategory")}
-                    />
-                </div>
-                <div className="flex">
-                    <input
-                        style={{ outline: "none" }}
-                        onClick={successTextRemover}
-                        className="mb-3 mr-3 w-2/5  flex-auto border-2 border-solid border-indigo-600 py-2 px-3"
-                        placeholder="Status"
-                        {...register("status")}
-                    />
-                    {projects?.length && (
-                        <select
-                            {...register("project")}
-                            className="mb-3 ml-3 w-2/5  flex-auto border-2 border-solid border-indigo-600 py-2 px-3"
-                        >
-                            {projects.map((project: any) => (
-                                <option value={project._id} key={project._id}>
-                                    {project.name}
-                                </option>
-                            ))}
-                        </select>
-                    )}
-                    {severity?.length && (
-                        <select
-                            {...register("severity")}
-                            className="mb-3 ml-3 w-2/5  flex-auto border-2 border-solid border-indigo-600 py-2 px-3"
-                        >
-                            {severity.map((s: any) => (
-                                <option value={s} key={s}>
-                                    {s}
-                                </option>
-                            ))}
-                        </select>
-                    )}
-                </div>
-                <textarea
-                    style={{ outline: "none" }}
-                    onClick={successTextRemover}
-                    className="mb-3 mr-3 w-full flex-auto border-2 border-solid border-indigo-600 py-2 px-3"
-                    placeholder="Bug Description"
-                    {...register("bugDescription")}
-                />
+                        <input
+                            style={{ outline: "none" }}
+                            onClick={successTextRemover}
+                            className="mb-3 mr-3 w-full flex-auto rounded-[3px] border-2 border-solid py-2 px-3 focus:border-[#22577E] md:w-2/5"
+                            placeholder="Bug Category"
+                            {...register("bugCategory")}
+                        />
+                    </div>
+                    <div className="flex flex-col md:flex-row">
+                        <input
+                            style={{ outline: "none" }}
+                            onClick={successTextRemover}
+                            className="mb-3 mr-3 w-full flex-auto rounded-[3px] border-2 border-solid py-2 px-3 focus:border-[#22577E] md:w-2/5"
+                            placeholder="Status"
+                            {...register("status")}
+                        />
+                        {projects?.length && (
+                            <select
+                                {...register("project")}
+                                className="mb-3 mr-3 w-full flex-auto rounded-[3px] border-2 border-solid py-2 px-3 focus:border-[#22577E] md:w-2/5"
+                            >
+                                {projects.map((project: any) => (
+                                    <option value={project._id} key={project._id}>
+                                        {project.name}
+                                    </option>
+                                ))}
+                            </select>
+                        )}
+                        {severity?.length && (
+                            <select
+                                {...register("severity")}
+                                className="mb-3 mr-3 w-full flex-auto rounded-[3px] border-2 border-solid py-2 px-3 focus:border-[#22577E] md:w-2/5"
+                            >
+                                {severity.map((s: any) => (
+                                    <option value={s} key={s}>
+                                        {s}
+                                    </option>
+                                ))}
+                            </select>
+                        )}
+                    </div>
+                    <div className="mx-1 flex flex-col md:flex-row">
+                        <textarea
+                            style={{ outline: "none" }}
+                            onClick={successTextRemover}
+                            className="mb-3 mr-3 w-full flex-auto rounded-[3px] border-2 border-solid py-2 px-3 focus:border-[#22577E]"
+                            placeholder="Bug Description"
+                            {...register("bugDescription")}
+                        />
+                    </div>
 
-                <button
-                    className="ml-auto flex w-auto rounded-lg bg-sky-600 px-9 py-4 text-center text-white"
-                    type="submit"
-                >
-                    Add Issue
-                </button>
-            </form>
+                    <button className="primary-btn" type="submit">
+                        Add Issue
+                    </button>
+                </form>
 
-            <p className="text-danger text-uppercase fs-4 mt-4 mb-5 pb-5 text-center">{success}</p>
+                <p className="text-danger text-uppercase fs-4 mt-4 mb-5 pb-5 text-center">
+                    {success}
+                </p>
+            </div>
         </div>
     );
 }
--- src/Homepage/Banner/Banner.tsx
+++ src/Homepage/Banner/Banner.tsx
@@ -1,13 +1,15 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable react/button-has-type */
 import bannerImage from "assets/images/banner.png";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function Banner() {
     return (
         <section>
-            <div className="mx-4 my-12 flex flex-col items-center justify-between py-10 md:max-w-3xl  lg:mx-auto lg:max-w-6xl lg:flex-row">
+            <div className="mx-4 my-4 flex flex-col items-center justify-between py-10 md:max-w-3xl  lg:mx-auto lg:max-w-6xl lg:flex-row">
                 {/* left side */}
                 <div className="order-2 mx-auto w-[90%] space-y-8 text-center lg:order-1 lg:w-[40%] lg:text-left">
                     <h1 className="text-4xl font-bold leading-[50px] text-gray-800">
@@ -25,9 +27,9 @@ function Banner() {
                             type="email"
                         />
                         <div className="flex-warp mx-auto flex items-center justify-center space-x-4 lg:justify-start">
-                            <button className="primary-btn rounded-lg py-3 px-7">
-                                Get Started
-                            </button>
+                            <Link href="/register">
+                                <a className="primary-btn">Get Started</a>
+                            </Link>
                             <p className="text-sm font-semibold uppercase text-gray-300">
                                 Free forever <br /> no credit card
                             </p>
@@ -72,7 +74,7 @@ function Banner() {
 
                 {/* right side  */}
                 <div className="order-1 w-full lg:order-2 lg:w-[60%]">
-                    <div className="bg-none bg-cover bg-center bg-no-repeat shadow-[0_0_10px_#5584AC]">
+                    <div className="bg-none bg-cover bg-center bg-no-repeat">
                         <Image src={bannerImage} />
                     </div>
                 </div>
