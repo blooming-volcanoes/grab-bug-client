fef8d2d18bc3259e0e49dd6d4bebfbf8daad8c1b
Sadique <mhsadique1307@gmail.com>
2022-04-23 19:45:09 +0600

    test commit

--- src/DashboardPages/Tickets/TicketDetailsList.tsx
+++ src/DashboardPages/Tickets/TicketDetailsList.tsx
@@ -3,6 +3,7 @@ import SerchTicket from "./SearchTicket";
 import TicketList from "./TicketList";
 
 function TicketDetailsList({ tickets }: any) {
+    console.log(tickets, "details");
     return (
         <div className="m-0 md:m-3">
             <div className="container mx-auto h-max rounded-[3px] shadow-[0_0_10px_#5584AC] md:p-3">
