149116cf95af1026cf7ac100d974fa9d9a6b0940
Sadique <mhsadique1307@gmail.com>
2022-04-28 04:39:36 +0600

    used destructuring in 'manageUserRoles/[projectId].tsx' file

--- src/pages/dashboard/projects/manageUserRoles/[projectId].tsx
+++ src/pages/dashboard/projects/manageUserRoles/[projectId].tsx
@@ -17,7 +17,7 @@ export const getServerSideProps: GetServerSideProps = async (context: any) => {
     const { params } = context;
     const { projectId } = params;
     const { users } = await UserHttpReq.getAllUsers();
-    const project = await ProjectHttpReq.getProject(projectId);
+    const { project } = await ProjectHttpReq.getProject(projectId);
     const roles = [
         "Project Manager",
         "Front-end Developer",
@@ -32,7 +32,7 @@ export const getServerSideProps: GetServerSideProps = async (context: any) => {
         props: {
             users,
             roles,
-            project: project.project,
+            project,
         },
     };
 };
