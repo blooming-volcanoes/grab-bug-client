551f7fd84a59cb6b2a6d8217eb6546f92b50086a
Sadique <mhsadique1307@gmail.com>
2022-05-05 23:42:02 +0600

    little change in TicketCommentSingle component

--- src/DashboardPages/TicketDetails/TicketCommentSingle.tsx
+++ src/DashboardPages/TicketDetails/TicketCommentSingle.tsx
@@ -6,8 +6,8 @@ function TicketCommentSingle({ comment }: any) {
             <td className="table-td">{comment.text}</td>
             <td className="table-td">{comment.commentedBy}</td>
             <td className="table-td">
-                {new Date(comment.createdAt).toDateString()}{" "}
-                {new Date(comment.createdAt).toDateString()}
+                {new Date(comment.createdAt).toLocaleDateString()}{" "}
+                {new Date(comment.createdAt).toLocaleTimeString()}
             </td>
         </tr>
     );
