918896cad3a9af0ae9b0b28e64746fa302441c49
Sadique <mhsadique1307@gmail.com>
2022-05-09 21:20:55 +0600

    rendering comments immediately after commenting has been implemented

--- src/DashboardPages/TicketDetails/TicketComments.tsx
+++ src/DashboardPages/TicketDetails/TicketComments.tsx
@@ -6,21 +6,38 @@
  *** Author : Raiyan
  */
 import cogoToast from "cogo-toast";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import IssueHttpReq from "services/Issue.service";
 import TicketCommentSingle from "./TicketCommentSingle";
 
 function TicketComments({ issue }: any) {
     const { _id } = issue;
-    const [comment, setComment] = useState("");
+    const [commentText, setCommentText] = useState("");
+    const [comments, setComments] = useState(issue.comments);
+    const [loading, setLoading] = useState(false);
     const handleComment = async () => {
-        const res = await IssueHttpReq.addComment(_id, comment);
-        if (res.data.success) {
-            setComment("");
-            cogoToast.success("Comment posted!");
-            window.location.reload();
+        setLoading(true);
+        try {
+            const res = await IssueHttpReq.addComment(_id, commentText);
+            if (res.data.success) {
+                setCommentText("");
+                cogoToast.success("Comment posted!");
+            }
+        } catch (err: any) {
+            console.log(err);
+            setLoading(false);
         }
+        setLoading(false);
     };
+
+    useEffect(() => {
+        const fetcher = async () => {
+            const issueData = await IssueHttpReq.getIssue(issue._id);
+            setComments(issueData.data.result.comments);
+        };
+        fetcher();
+    }, [loading]);
+
     return (
         <div>
             {/* ticket comments */}
@@ -53,7 +70,7 @@ function TicketComments({ issue }: any) {
                                                 </tr>
                                             </thead>
                                             <tbody>
-                                                {issue?.comments?.map((com: any) => (
+                                                {comments?.map((com: any) => (
                                                     <TicketCommentSingle
                                                         key={com._id}
                                                         comment={com}
@@ -75,10 +92,10 @@ function TicketComments({ issue }: any) {
             <h1 className="mt-5 text-xl">Write a comment here</h1>
             <div className="my-5 flex flex-col space-x-0 space-y-4 lg:flex-row lg:space-y-0 lg:space-x-4">
                 <textarea
-                    onChange={(e) => setComment(e.target.value)}
+                    onChange={(e) => setCommentText(e.target.value)}
                     rows={7}
                     className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-[#22577E] focus:ring-[#22577E] sm:text-sm"
-                    value={comment}
+                    value={commentText}
                 />
                 <button
                     className="primary-btn max-h-14 w-full lg:w-[30%]"
