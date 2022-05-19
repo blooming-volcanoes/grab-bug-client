a8efb1fc298ad84a9aa1b85a2db5abe71009acf5
Sadique <mhsadique1307@gmail.com>
2022-05-19 11:11:32 +0600

    done testing

--- src/pages/dashboard/tickets/details/[ticketId].tsx
+++ src/pages/dashboard/tickets/details/[ticketId].tsx
@@ -11,6 +11,7 @@ import React from "react";
 import IssueHttpReq from "services/Issue.service";
 
 function TicketDetails({ issue }: { issue: any }) {
+    console.log("small change");
     return (
         <DashboardLayout>
             <Main issue={issue} />
