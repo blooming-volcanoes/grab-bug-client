8ab4ff7b3d7e49cf34277436cd3ab7d323574406
Raiyan <raiyan5764@gmail.com>
2022-05-20 00:24:59 +0600

    Some modification done

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,18 @@
+import Cards from "Chart/Cards/Cards";
 import DashboardLayout from "Layouts/DashboardLayout";
 import React from "react";
 
 function Dashboard() {
     return (
         <DashboardLayout>
-            <h1 className=" text-center">Main dashboard</h1>
+            <div className="App">
+                <div className="AppGlass">
+                    <div className="MainDash">
+                        <h1 className="text-2xl font-semibold">Dashboard</h1>
+                        <Cards />
+                    </div>
+                </div>
+            </div>
         </DashboardLayout>
     );
 }
