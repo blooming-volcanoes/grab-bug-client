74757becdc5032ad26736560425c295d5e353e0f
Raiyan <raiyan5764@gmail.com>
2022-05-20 13:22:32 +0600

    animated chart done

--- src/Chart/Card/Card.tsx
+++ src/Chart/Card/Card.tsx
@@ -8,7 +8,7 @@ import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import { ImCross } from "react-icons/im";
 
-const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
+const ReactApexChart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 function Card({ card }: { card: any }) {
     const [expanded, setExpanded] = useState(false);
@@ -60,57 +60,6 @@ function ExpandedCard({
     card: any;
     setExpanded: React.Dispatch<React.SetStateAction<boolean | React.MouseEvent>>;
 }) {
-    const data = {
-        options: {
-            chart: {
-                type: "area",
-                height: "auto",
-            },
-
-            dropShadow: {
-                enabled: false,
-                enabledOnSeries: undefined,
-                top: 0,
-                left: 0,
-                blur: 3,
-                color: "#000",
-                opacity: 0.35,
-            },
-
-            fill: {
-                colors: ["#fff"],
-                type: "gradient",
-            },
-            dataLabels: {
-                enabled: false,
-            },
-            stroke: {
-                curve: "smooth",
-                colors: ["white"],
-            },
-            tooltip: {
-                x: {
-                    format: "dd/MM/yy HH:mm",
-                },
-            },
-            grid: {
-                show: true,
-            },
-            xaxis: {
-                type: "datetime",
-                categories: [
-                    "2018-09-19T00:00:00.000Z",
-                    "2018-09-19T01:30:00.000Z",
-                    "2018-09-19T02:30:00.000Z",
-                    "2018-09-19T03:30:00.000Z",
-                    "2018-09-19T04:30:00.000Z",
-                    "2018-09-19T05:30:00.000Z",
-                    "2018-09-19T06:30:00.000Z",
-                ],
-            },
-        },
-    };
-
     return (
         <motion.div
             style={{ background: card.color.backGround, boxShadow: card.color.boxShadow }}
@@ -122,7 +71,7 @@ function ExpandedCard({
             </div>
             <span>{card.title}</span>
             <div className="chartContainer">
-                <Chart options={data.options} series={card.series} type="area" />
+                <ReactApexChart options={card.options} series={card.series} type="area" />
             </div>
             <span>Last 24 hours</span>
         </motion.div>
--- src/Chart/Cards/Cards.tsx
+++ src/Chart/Cards/Cards.tsx
@@ -17,6 +17,54 @@ const cardData = [
                 data: [31, 40, 28, 51, 42, 109, 100],
             },
         ],
+        options: {
+            chart: {
+                type: "area",
+                height: "auto",
+            },
+
+            dropShadow: {
+                enabled: false,
+                enabledOnSeries: undefined,
+                top: 0,
+                left: 0,
+                blur: 3,
+                color: "#000",
+                opacity: 0.35,
+            },
+
+            fill: {
+                colors: ["#fff"],
+                type: "gradient",
+            },
+            dataLabels: {
+                enabled: false,
+            },
+            stroke: {
+                curve: "smooth",
+                colors: ["white"],
+            },
+            tooltip: {
+                x: {
+                    format: "dd/MM/yy HH:mm",
+                },
+            },
+            grid: {
+                show: true,
+            },
+            xaxis: {
+                type: "datetime",
+                categories: [
+                    "2018-09-19T00:00:00.000Z",
+                    "2018-09-19T01:30:00.000Z",
+                    "2018-09-19T02:30:00.000Z",
+                    "2018-09-19T03:30:00.000Z",
+                    "2018-09-19T04:30:00.000Z",
+                    "2018-09-19T05:30:00.000Z",
+                    "2018-09-19T06:30:00.000Z",
+                ],
+            },
+        },
     },
     {
         title: "Tickets",
@@ -32,6 +80,54 @@ const cardData = [
                 data: [10, 100, 50, 70, 80, 30, 40],
             },
         ],
+        options: {
+            chart: {
+                type: "area",
+                height: "auto",
+            },
+
+            dropShadow: {
+                enabled: false,
+                enabledOnSeries: undefined,
+                top: 0,
+                left: 0,
+                blur: 3,
+                color: "#000",
+                opacity: 0.35,
+            },
+
+            fill: {
+                colors: ["#fff"],
+                type: "gradient",
+            },
+            dataLabels: {
+                enabled: false,
+            },
+            stroke: {
+                curve: "smooth",
+                colors: ["white"],
+            },
+            tooltip: {
+                x: {
+                    format: "dd/MM/yy HH:mm",
+                },
+            },
+            grid: {
+                show: true,
+            },
+            xaxis: {
+                type: "datetime",
+                categories: [
+                    "2018-09-19T00:00:00.000Z",
+                    "2018-09-19T01:30:00.000Z",
+                    "2018-09-19T02:30:00.000Z",
+                    "2018-09-19T03:30:00.000Z",
+                    "2018-09-19T04:30:00.000Z",
+                    "2018-09-19T05:30:00.000Z",
+                    "2018-09-19T06:30:00.000Z",
+                ],
+            },
+        },
     },
     {
         title: "On Going",
@@ -47,6 +143,54 @@ const cardData = [
                 data: [10, 25, 15, 30, 12, 15, 20],
             },
         ],
+        options: {
+            chart: {
+                type: "area",
+                height: "auto",
+            },
+
+            dropShadow: {
+                enabled: false,
+                enabledOnSeries: undefined,
+                top: 0,
+                left: 0,
+                blur: 3,
+                color: "#000",
+                opacity: 0.35,
+            },
+
+            fill: {
+                colors: ["#fff"],
+                type: "gradient",
+            },
+            dataLabels: {
+                enabled: false,
+            },
+            stroke: {
+                curve: "smooth",
+                colors: ["white"],
+            },
+            tooltip: {
+                x: {
+                    format: "dd/MM/yy HH:mm",
+                },
+            },
+            grid: {
+                show: true,
+            },
+            xaxis: {
+                type: "datetime",
+                categories: [
+                    "2018-09-19T00:00:00.000Z",
+                    "2018-09-19T01:30:00.000Z",
+                    "2018-09-19T02:30:00.000Z",
+                    "2018-09-19T03:30:00.000Z",
+                    "2018-09-19T04:30:00.000Z",
+                    "2018-09-19T05:30:00.000Z",
+                    "2018-09-19T06:30:00.000Z",
+                ],
+            },
+        },
     },
 ];
 
