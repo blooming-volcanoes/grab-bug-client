7805d45615c7f78b82bcf996d00387b9d5d143d2
Sadique <mhsadique1307@gmail.com>
2022-05-24 16:25:44 +0600

    used optional chaining

--- src/components/MyProjects/MyProjectSingle.tsx
+++ src/components/MyProjects/MyProjectSingle.tsx
@@ -13,17 +13,19 @@ function MyProjectSingle({ projectId }: any) {
         };
         fetcher();
     }, []);
-    console.log(project);
+    console.log(project, "PROJECT");
     return (
         <div className="m-0  md:m-3">
             <div className="rounded-[3px] bg-gradient-to-r from-[#a7d1f0] via-[#5584AC] to-[#22577E] p-4">
                 <h2 className="text-2xl font-bold uppercase text-[#FAFFAF]">
-                    Project Name: {project.name}
+                    Project Name: {project?.name}
                 </h2>
-                <p className="text-base">Deadline: {new Date(project.deadline).toLocaleString()}</p>
-                <p className="text-gray-900">Description: {project.description}</p>
+                <p className="text-base">
+                    Deadline: {new Date(project?.deadline).toLocaleString()}
+                </p>
+                <p className="text-gray-900">Description: {project?.description}</p>
                 <button
-                    onClick={() => router.push(`/dashboard/issueBoard/${project._id}`)}
+                    onClick={() => router.push(`/dashboard/issueBoard/${project?._id}`)}
                     className="primary-btn"
                     type="button"
                 >
